85c098e7298cc1633bb42c25de20370d3a97cf19
waleed018 <waleed.abdullah01700@gmail.com>
2025-09-08 17:58:47 +0500

    test server

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get("/healthz", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
